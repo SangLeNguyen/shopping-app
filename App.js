68548544b28cae7a7fa36184cc3649e836dc0546
Sang <phusang2501@gmail.com>
2021-07-13 14:36:46 +0300

    second update

--- App.js
+++ App.js
@@ -4,13 +4,19 @@ import { createStore, combineReducers } from 'redux';
 import {Provider} from 'react-redux';
 
 import productsReducer from './store/reducers/products';
+import ShopNavigator from './navigation/ShopNavigator';
 
+const rootReducer = combineReducers({
+  products: productsReducer  
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open your app!</Text>
-    </View>
+    <Provider store={store}>
+      <ShopNavigator/>
+    </Provider>
   );
 }
 
