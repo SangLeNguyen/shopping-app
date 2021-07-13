b20836bb7f42dce7da1bbccd2612340cf71ed501
Sang <phusang2501@gmail.com>
2021-07-13 11:16:12 +0300

    first update

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import {Provider} from 'react-redux';
+
+import productsReducer from './store/reducers/products';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Open your app!</Text>
     </View>
   );
 }
